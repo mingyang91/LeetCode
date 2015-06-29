b0bd8434e174edac8362db9bfc6a7e9d737dd187
明扬 <mingyang91@qq.com>
2015-06-29 18:47:31 +0800

    尝试第三种方式

--- 1. Two Sum/index.js
+++ 1. Two Sum/index.js
@@ -28,8 +28,8 @@ var twoSum1 = function (nums, target) {
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
-
+var twoSum2 = function (nums, target) {
+  var track = Date.now();
   var list = nums
     .map(function (number, index) {
       return {number: number, index: index};
@@ -37,12 +37,13 @@ var twoSum = function (nums, target) {
     .sort(function (a, b) {
       return a.number - b.number;
     });
-
+  console.log(Date.now() - track);
   //console.log(list);
-
+  track = Date.now();
   for (var leftCursor = 0, rightCursor = list.length - 1; ; ) {
     var current = list[leftCursor].number + list[rightCursor].number;
     if (current === target) {
+      console.log(Date.now() - track);
       return list[leftCursor].index > list[rightCursor].index ?
         [ list[rightCursor].index + 1, list[leftCursor].index + 1 ] :
         [ list[leftCursor].index + 1, list[rightCursor].index + 1 ];
@@ -54,4 +55,38 @@ var twoSum = function (nums, target) {
   }
 };
 
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  var max = nums.reduce(function (max, next) { return max > next ? max : next; });
+  var min = nums.reduce(function (min, next) { return min < next ? min : next; });
+  //var track = Date.now();
+  var numsNew = {};
+  nums
+    .map(function (number, index) {
+      numsNew[number] = index;
+    });
+  //console.log(Date.now() - track);
+  //console.log(list);
+  //track = Date.now();
+  for (var leftCursor = min, rightCursor = max; ; ) {
+    var current = leftCursor + rightCursor;
+    if (leftCursor === rightCursor) { return; }
+
+    if (current === target) {
+      //console.log(Date.now() - track);
+      return numsNew[leftCursor] > numsNew[rightCursor] ?
+        [ numsNew[rightCursor] + 1, numsNew[leftCursor] + 1 ] :
+        [ numsNew[leftCursor] + 1, numsNew[rightCursor] + 1 ];
+    } else if (current > target) {
+      while(numsNew[--rightCursor] === undefined);
+    } else if (current < target) {
+      while(numsNew[++leftCursor] === undefined);
+    }
+  }
+};
 module.exports = twoSum;
