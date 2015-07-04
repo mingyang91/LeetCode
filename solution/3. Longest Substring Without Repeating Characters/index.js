a0af52c666ca49ee57631ac0b8dbe1ea7b7d417f
famer.me <mingyang91@qq.com>
2015-07-04 11:27:23 +0800

    第三题，性能有所优化，但是还是挂在一个超大的测试用例中 顺便做了反转二叉树，还没放到代码里

--- solution/3. Longest Substring Without Repeating Characters/index.js
+++ solution/3. Longest Substring Without Repeating Characters/index.js
@@ -2,9 +2,11 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring1 = function(s) {
   var substr, subStringList = [];
   var longLength = 0;
+  var offsetList = [];
+
   for (var length = 1; length <= s.length / 2; length++) {
     for (var offset = 0; offset < s.length - length; offset++) {
       substr = s.substr(offset, length);
@@ -40,6 +42,52 @@ var lengthOfLongestSubstring = function(s) {
   return longLength;
 };
 
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  var substr, subStringList = [];
+  var longLength = 0;
+  var offsetList = [];
+
+  for (var offset = 0; offset < s.length / 2; offset++) {
+    offsetList.push(offset);
+  }
+  for (var length = 1; length <= s.length / 2; length++) {
+    offsetList = offsetList.filter(function (offset) {
+      substr = s.substr(offset, length);
+      if (s.indexOf(substr) !== s.lastIndexOf(substr)) {
+        var repeat = subStringList.some(function (sub) {
+          if (sub === substr) {
+            return true;
+          }
+
+          for (var substrIndex = 0; substrIndex < substr.length; substrIndex++) {
+            if (sub[substrIndex % sub.length] !== substr[substrIndex]) {
+              return false;
+            }
+          }
+          return true;
+        });
+        if (!repeat) {
+          subStringList.push(substr);
+        }
+        return !repeat;
+      } else {
+       return false;
+      }
+    });
+
+  }
+
+
+  if (subStringList.length > 0)
+  longLength = subStringList.reduce(function (before, now) {
+    return now.length > before ? now.length : before;
+  }, 0);
+  return longLength;
+};
 
 
 
