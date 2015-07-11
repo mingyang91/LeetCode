783a9fdf0fc6f853541c2f15a53c8e182319b01d
明扬 <mingyang91@qq.com>
2015-07-11 21:33:36 +0800

    未完成的解法 要对边界条件判断很多

--- solution/4. Median of Two Sorted Arrays/index.js
+++ solution/4. Median of Two Sorted Arrays/index.js
@@ -7,54 +7,110 @@
 var findMedianSortedArrays = function(nums1, nums2) {
   "use strict";
 
-  var index1 = (nums1.length - 1) / 2;
-  var index2 = (nums2.length - 1) / 2;
-  var startingDirection = nums1[index1] > nums2[index2];
 
-  //for (;;) {
-  //  if (startingDirection === nums1[index1] > nums2[index2]) {
-  //    //TODO 处理边界情况
-  //    index1--;
-  //    index2++;
-  //  } else {
-  //
-  //  }
-  //}
+  var index1 = getHalfIndexes(nums1);
+  var index2 = getHalfIndexes(nums2);
+  var startingDirection = compareInterval([nums1[index1[0]], nums1[index1[1]]], [nums2[index2[0]], nums2[index2[1]]]);
+  if (startingDirection > 0) {
+    startingDirection = 1;
+  } else if (startingDirection < 0) {
+    startingDirection = -1;
+  } else {
+    return getMedianForTwoInterval([nums1[index1[0]], nums1[index1[1]]], [nums2[index2[0]], nums2[index2[1]]]);
+  }
+
+  for (; ; ) {
+    var nowDirection = compareInterval([nums1[index1[0]], nums1[index1[1]]], [nums2[index2[0]], nums2[index2[1]]]);
+    if (nowDirection > 0) {
+      nowDirection = 1;
+    } else if (nowDirection < 0) {
+      nowDirection = -1;
+    } else {
+      nowDirection = 0;
+    }
+    if (nowDirection === startingDirection) {
+      //越界
+
+      if (isOutOfIndex(index1[0] - startingDirection, nums1) ||
+          isOutOfIndex(index1[1] - startingDirection, nums1) ||
+          isOutOfIndex(index2[0] + startingDirection, nums2) ||
+          isOutOfIndex(index2[1] + startingDirection, nums2)) {
+        return getMedianForTwoInterval([nums1[index1[0]], nums1[index1[1]]], [nums2[index2[0]], nums2[index2[1]]]);
+      }
+
+      index1[0] -= startingDirection;
+      index1[1] -= startingDirection;
+      index2[0] += startingDirection;
+      index2[1] += startingDirection;
+    } else if (nowDirection === 0) {
+      return getMedianForTwoInterval([nums1[index1[0]], nums1[index1[1]]], [nums2[index2[0]], nums2[index2[1]]]);
+    } else {
+      index1[0] += startingDirection;
+      index1[1] += startingDirection;
+      index2[0] -= startingDirection;
+      index2[1] -= startingDirection;
+      return getMedianForTwoInterval([nums1[index1[0]], nums1[index1[1]]], [nums2[index2[0]], nums2[index2[1]]]);
+    }
+  }
 };
 
-var getMixValue = function (arr, index) {
-  "use strict";
-  if (Number.isInteger(index)) {
-    return arr[index];
+var isOutOfIndex = function (index, arr) {
+  return index < 0 || index >= arr.length;
+};
+
+var getHalfIndexes = function (arr) {
+  if (arr.length === 0) {
+
+
+    return [NaN, NaN];
   } else {
-    return arr[Math.floor(index)] + arr[Math.ceil(index)] * (index - Math.ceil(index));
+
+
+    var halfIndex = (arr.length - 1) / 2;
+    return [Math.floor(halfIndex), Math.ceil(halfIndex)];
   }
 };
 
 var getMedianForTwoInterval = function (arr1, arr2) {
   "use strict";
-  if (arr1.length === 1) {
-    arr1[1] = arr1[0];
-  }
+  //if (arr1.length === 1) {
+  //  arr1[1] = arr1[0];
+  //}
+  //
+  //if (arr2.length === 1) {
+  //  arr2[1] = arr2[0];
+  //}
 
-  if (arr2.length === 1) {
-    arr2[1] = arr2[0];
-  }
-
-  if (arr1[0] > arr2[arr2.length - 1] || arr1[arr1.length - 1] < arr2[0]) {
-    return false;
-  } else {
+  //if (arr1[0] > arr2[arr2.length - 1] || arr1[arr1.length - 1] < arr2[0]) {
+  //if (compareInterval(arr1, arr2)) {
+  //  return false;
+  //} else {
     //无交集
-    var sorted = arr1.concat(arr2).sort();
-    return (sorted[1] + sorted[2]) / 2;
+    var sorted = arr1.concat(arr2).sort().filter(function (e) { return e !== undefined; });
+    if (sorted.length === 4) {
+      return (sorted[1] + sorted[2]) / 2;
+    } else {
+      return (sorted[0] + sorted[1]) / 2;
+    }
+  //}
+};
+
+var compareInterval = function (arr1, arr2) {
+  var greate = arr1[0] - arr2[arr2.length - 1];
+  var less = arr1[arr1.length - 1] - arr2[0];
+  if (greate > 0) {
+    return arr1[0] - arr2[arr2.length - 1];
+  } else if (less < 0) {
+    return arr1[arr1.length - 1] - arr2[0];
+  } else {
+    return 0;
   }
-
-
 };
 
 
 
 module.exports = {
   findMediaSortedArrays: findMedianSortedArrays,
-  getMedianForTwoInterval: getMedianForTwoInterval
+  getMedianForTwoInterval: getMedianForTwoInterval,
+  compareInterval: compareInterval
 };
